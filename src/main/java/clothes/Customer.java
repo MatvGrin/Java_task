4b24765bb0c45c142e064b4d9b4c88c7d5217b89
Matvii_Hrinchenko <matvii.hrinchenko@gmail.com>
2023-08-24 13:56:33 +0200

    add new code

---
+++ src/main/java/clothes/Customer.java
@@ -0,0 +1,83 @@
+package clothes;
+
+public class Customer {
+    //Створити класс Customer: id, name, surname, address (адреса),
+    // credit card number(номер кредитної картимарка), account number (номер банківського рахунку).
+
+    private long id;
+    private String name;
+    private String surname;
+    private Address address;
+    private int creditCardNumber;
+    private int accountNumber;
+
+    public Customer(long id, String name, String surname, Address address, int creditCardNumber, int accountNumber) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.creditCardNumber = creditCardNumber;
+        this.accountNumber = accountNumber;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public int getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(int creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", address='" + address + '\'' +
+                ", creditCardNumber=" + creditCardNumber +
+                ", accountNumber=" + accountNumber +
+                '}';
+    }
+}
---
+++ src/main/java/clothes/CustomerDemo.java
@@ -0,0 +1,31 @@
+package clothes;
+
+public class CustomerDemo {
+    public static void main(String[] args) {
+        Address address1 = new Address(21000,"Split","Zala",12);
+        Address address2 = new Address(25021,"Vinnitsa","Korolenko",35);
+        Address address3 = new Address(95632,"Paris","Sandjmen",18);
+        Address address4 = new Address(15328,"Amsterdam","Pikaso",26);
+
+        Address[] addresses = {address1,address2,address3,address4};
+
+        Customer customer1 = new Customer(1,"Piter","Volker", address1, 4825,59);
+        Customer customer2 = new Customer(2,"Lex","Alker", address2, 8965,98);
+        Customer customer3 = new Customer(3,"Mark","Diper", address3, 2952,2);
+        Customer customer4 = new Customer(4,"Luck","Menderi", address4, 3456,136);
+
+        Customer[] customers = {customer1,customer2,customer3,customer4};
+
+        CustomerService service = new CustomerService(customers);
+
+        service.showAllCustomers();
+        service.showCustomersByCity("Paris");
+        service.showCustomersByCreditCard(3000, 6000);
+
+        System.out.println(service.findCustomerById(3));
+        System.out.println(service.findCustomerByAddress(address4));
+        System.out.println(service.findCustomerByAccount(98));
+
+
+    }
+}
