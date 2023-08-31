79046037ad837a5b9dcc61079123a8dbcaeabe04
Matvii_Hrinchenko <matvii.hrinchenko@gmail.com>
2023-08-31 20:55:13 +0200

    add new code

--- src/main/java/numb/Number.java
+++ src/main/java/numb/Number.java
@@ -8,22 +8,63 @@ public class Number {
         this.array = array;
     }
 
-//    private int avarice(int num){
-//        for (int i = 0; i < array.length; i++) {
-//            array[i] = i+1;
-//        }
-//        return ;
-//    }
-//
     public void startAndEnd(int start, int end){
         int temp = 0;
+        int len = 0;
         for (int i : array) {
             if (i >= start && i <= end){
-                temp++;
+                temp += i;
+                len++;
             }
         }
+        int rez = temp / len;
+        System.out.println(rez);
     }
 
+    public int elementsOfArithmeticProgression(){
+        int d = 0;
+        for (int i = 0; i < array.length; i++) {
+            d = array[i+1] - array[i];
+            if (array[i]+d != array[i+1]){
+                return 0;
+            }
+        }
+        return d;
+    }
+
+    public int elementsOfGeometricProgression(){
+        int b = 0;
+        for (int i = 0; i < array.length; i++) {
+            b = array[i+1] / array[i];
+            if (array[i]*3 != array[i+1]){
+                return 0;
+            }
+        }
+        return b;
+    }
+
+    public int positiveAndNegativeNumbers(){
+        System.out.print("Positive and negative numbers: ");
+        boolean temp = array[0] > 0;
+        for (int i = 0; i < array.length; i++) {
+            if (temp) {
+                if (array[i] < 0) return i;
+                temp = false;
+            } else {
+                if (array[i] > 0) return i;
+                temp = true;
+            }
+        }
+        return 0;
+    }
+
+    public void minimalElementAmongAllElements(){
+
+    }
+
+    public void theMinimumOfTheLocalMaxima(){
+
+    }
 
 
 }
--- src/main/java/numb/UserAccount.java
+++ src/main/java/numb/UserAccount.java
@@ -1,7 +1,5 @@
 package numb;
 
-import numbers.Number;
-
 import java.util.Scanner;
 
 public class UserAccount {
@@ -16,30 +14,49 @@ public class UserAccount {
                 System.out.print("Input numbers " + i + ": ");
                 numbers[i] = scanner.nextInt();
             }
-            numbers.Number number = new Number(numbers);
+            Number number = new Number(numbers);
 
             System.out.println("startAndEnd -> 1");
-            System.out.println(" -> 2");
-
-            System.out.println("get out -> 9");
+            System.out.println("elementsOfArithmeticProgression -> 2");
+            System.out.println("elementsOfGeometricProgression -> 3");
+            System.out.println("positiveAndNegativeNumbers -> 4");
+            System.out.println("minimalElementAmongAllElements -> 5");
+            System.out.println("theMinimumOfTheLocalMaxima -> 6");
+            System.out.println("get out -> 10");
 
             System.out.print("Input number metod: ");
             int metod = scanner.nextInt();
 
             switch (metod){
                 case 1: {
-//                    number.startAndEnd();
+                    number.startAndEnd(2,99);
                     break;
                 }
                 case 2: {
-//                    number.();
+                    System.out.println(number.elementsOfArithmeticProgression());
+                    break;
+                }
+                case 3: {
+                    System.out.println(number.elementsOfGeometricProgression());
+                    break;
+                }
+                case 4: {
+                    System.out.println(number.positiveAndNegativeNumbers());
+                    break;
+                }
+                case 5: {
+                    number.minimalElementAmongAllElements();
+                    break;
+                }
+                case 6: {
+                    number.theMinimumOfTheLocalMaxima();
                     break;
                 }
                 default: {
                     System.out.println("Error");
                 }
             }
-            if (metod == 9){
+            if (metod == 7){
                 break;
             }
 
