32bde7413414daa64ab4fc1123947aa0916047c0
Matvii_Hrinchenko <matvii.hrinchenko@gmail.com>
2023-08-31 21:38:48 +0200

    add new code

--- src/main/java/numb/Number.java
+++ src/main/java/numb/Number.java
@@ -23,7 +23,7 @@ public class Number {
 
     public int elementsOfArithmeticProgression(){
         int d = 0;
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             d = array[i+1] - array[i];
             if (array[i]+d != array[i+1]){
                 return 0;
